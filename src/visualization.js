3b33b2f816913544c865ca05277689ba86f10674
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2019-12-16 10:59:25 -0500

    Update and rename visulization.js to visualization.js

--- src/visualization.js
+++ src/visualization.js
@@ -69,10 +69,10 @@ export const getFileContent = async (allIds) => {
     generateBarChart(cf, jsonData, 'ageInt', 'dataSummaryVizChart3', 'dataSummaryVizLabel3', 'selectedRange3', 'chartDiv3');
 
     dc.config.defaultColors(d3.schemeBrBG[4]);
-    renderPieChart(cf, jsonData, 'consortium', 'dataSummaryVizChart7', 'dataSummaryVizLabel7', 'selectedRange7', 'chartDiv7');
+    renderPieChart(cf, jsonData, 'Consortium', 'dataSummaryVizChart7', 'dataSummaryVizLabel7', 'selectedRange7', 'chartDiv7');
     generateBarSingleSelect(cf, 'famHist', 'dataSummaryVizChart6', 'dataSummaryVizLabel6', 'selectedRange6', 'chartDiv6')
     dc.config.defaultColors(d3.schemeBrBG[8]);
-    renderPieChart(cf, jsonData, 'study', 'dataSummaryVizChart1', 'dataSummaryVizLabel1', 'selectedRange1', 'chartDiv1');
+    renderPieChart(cf, jsonData, 'Study', 'dataSummaryVizChart1', 'dataSummaryVizLabel1', 'selectedRange1', 'chartDiv1');
     dc.config.defaultColors(d3.schemeSet2);
     renderPieChart(cf, jsonData, 'status', 'dataSummaryVizChart2', 'dataSummaryVizLabel2', 'selectedRange2', 'chartDiv2');
     dc.config.defaultColors(d3.schemeAccent);
